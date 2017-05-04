2ac36f4e21935ccc5bc807a3ef9db3a859abccc0
Anton Garder <avgarder@gmail.com>
2017-05-04 11:19:24 +0300

    new extractor

--- AquaFourier/app/src/main/java/ru/spbau/aquafourier/MainActivity.java
+++ AquaFourier/app/src/main/java/ru/spbau/aquafourier/MainActivity.java
@@ -20,6 +20,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
